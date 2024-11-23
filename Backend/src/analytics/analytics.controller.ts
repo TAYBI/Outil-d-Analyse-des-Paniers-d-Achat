425319ae35f11be5d6fe1b87f3c6c12f8678756e
Bilal <taybib4@gmail.com>
2024-11-23 13:08:48 +0100

    C:/Program Files/Git/total_sales total sales for a time period

--- Backend/src/analytics/analytics.controller.ts
+++ Backend/src/analytics/analytics.controller.ts
@@ -1,8 +1,46 @@
 import { Request, Response } from "express";
 import { IProduct } from "../types";
 import { Product } from "../models/product.model";
+import { Sale } from "../models/sale.model";
 
 class AnalyticsController {
+  async getTotalSales(req: Request, res: Response) {
+    try {
+      type TotalSales = { totalAmount: number };
+
+      const startDate = req.query.startDate
+        ? new Date(req.query.startDate as string)
+        : new Date(0);
+
+      const endDate = req.query.endDate
+        ? new Date(req.query.endDate as string)
+        : new Date();
+
+      console.log(startDate, endDate);
+
+      const totalSales: TotalSales[] = await Sale.aggregate([
+        {
+          $match: { Date: { $gte: startDate, $lte: endDate } },
+        },
+        {
+          $group: {
+            _id: null,
+            totalAmount: { $sum: "$TotalAmount" },
+          },
+        },
+      ]);
+
+      res.json({
+        success: true,
+        data: totalSales[0]?.totalAmount || 0,
+      });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ success: false, message: "Error fetching sales", error });
+    }
+  }
+
   async getProducts(req: Request, res: Response) {
     try {
       type ProductWithSales = IProduct & { totalSales: number };
--- Backend/src/analytics/analytics.route.ts
+++ Backend/src/analytics/analytics.route.ts
@@ -3,6 +3,7 @@ import AnalyticsController from "./analytics.controller";
 
 const router: Router = Router();
 
+router.get("/analytics/total_sales", AnalyticsController.getTotalSales);
 router.get("/products", AnalyticsController.getProducts);
 
 export default router;
