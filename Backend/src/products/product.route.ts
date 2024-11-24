7f219033e589a1f207bc5c1b50684badd3ac6cdc
Bilal <taybib4@gmail.com>
2024-11-24 10:22:37 +0100

    setup cors & helmets & rate limit

--- Backend/src/products/product.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import ProductController from "./product.controller";
-
-const router: Router = Router();
-
-router.get("/", ProductController.getProducts);
-
-export default router;
--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -2,11 +2,23 @@ import express, { Application } from "express";
 import routes from "./analytics/analytics.route";
 import mongoose from "mongoose";
 import "dotenv/config";
+import cors from "cors";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 const PORT: number = 3000;
 const MONGO_URI: string = process.env.MONGO_URI || "";
 const app: Application = express();
 
+app.use(cors());
+app.use(helmet());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+app.use(limiter);
+app.use(express.json());
 app.use("/", routes);
 
 mongoose.connect(MONGO_URI).then(() => {
